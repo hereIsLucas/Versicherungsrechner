e8c006e090466e94209ed96c02de45790f8aa82d
hereIsLucas <lucas.heroin@lernende.bbw.ch>
2023-05-11 00:18:58 +0200

    deleted old forms .js

---
+++ rechner/src/pages/AboutUs.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <h1>About Our Company</h1>
+      <p>We are a leading car insurance company, dedicated to providing affordable and reliable coverage to drivers of all types of vehicles. Our mission is to make sure that our clients are protected on the road, and that they feel confident in the insurance they have chosen.</p>
+      <h2>Our History</h2>
+      <p>Our company was founded over 30 years ago by a group of car enthusiasts who wanted to make sure that everyone had access to high-quality car insurance. Since then, we have grown into one of the most trusted names in the industry, and we continue to expand our coverage and services to meet the needs of our clients.</p>
+      <h2>Our Services</h2>
+      <p>At our company, we offer a wide range of insurance services for cars and other vehicles. Our policies cover everything from basic liability to comprehensive coverage, and we work with our clients to customize their policies to meet their specific needs and budget.</p>
+      <p>In addition to our insurance services, we also offer a variety of resources and tools to help our clients stay safe on the road. Our website features articles and videos on safe driving practices, as well as tips for maintaining your vehicle and getting the most out of your insurance policy.</p>
+      <h2>Our Commitment</h2>
+      <p>At our company, we are committed to providing the highest level of customer service and support. Our team of experienced insurance professionals is always available to answer questions and provide guidance, and we work tirelessly to make sure that our clients are satisfied with the coverage they have chosen.</p>
+      <p>Thank you for considering our company for your car insurance needs. We look forward to serving you!</p>
+    </div>
+  );
+};
+
+export default AboutUs;
